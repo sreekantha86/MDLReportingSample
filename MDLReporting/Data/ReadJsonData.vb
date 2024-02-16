11e38103a72261b22d8153a2118990d5c6bda9c1
Sreekanth A <sreekanth@git.ch>
2024-02-16 14:51:49 +0530

    Add project files.

---
+++ MDLReporting/Data/ReadJsonData.vb
@@ -0,0 +1,13 @@
+﻿Imports System.IO
+Imports Newtonsoft.Json
+
+Public Class ReadJsonData
+    Public Function ReadReportModel(ByVal path As String) As ReportModel
+        Dim reportData As New ReportModel
+        If File.Exists(path) Then
+            Dim jsonData As String = File.ReadAllText(path)
+            reportData = JsonConvert.DeserializeObject(Of ReportModel)(jsonData)
+        End If
+        Return reportData
+    End Function
+End Class
---
+++ MDLReporting/Form1.vb
@@ -0,0 +1,12 @@
+﻿Imports Newtonsoft.Json
+Imports System.IO
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            Dim jsonFilePath = Path.Combine(Application.StartupPath, "JSON Files", "10094547.json")
+            Dim reportData = New ReadJsonData().ReadReportModel(jsonFilePath)
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+End Class
